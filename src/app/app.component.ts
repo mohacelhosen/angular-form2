e82d71aa9376d22d76f59ddc327316f42121dbfe
mohacelhosen <mohacel.hosen22@gmail.com>
2023-06-07 09:43:05 +0600

    Angular Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form2';
+
+  signUp= new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(18), Validators.minLength(8)])
+  })
+
+  getControl(name:any):AbstractControl | null{
+    return this.signUp.get(name)
+  }
+
+  registrationFn(){
+    console.log(this.signUp.value);
+  }
 }
